f98d8dcf57466124d183eb12bbd6cd651059ec56
jstojek <108827118+jstojek@users.noreply.github.com>
2024-01-03 14:52:58 +0100

    Create EnemyController.cs

---
+++ EnemyController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using FMODUnity;
+
+namespace Platformer.Mechanics
+{
+    [RequireComponent(typeof(AnimationController), typeof(Collider2D))]
+    public class EnemyController : MonoBehaviour
+    {
+        public PatrolPath path;
+        public StudioEventEmitter ouchEmitter; // FMOD component for ouch audio playback.
+
+        internal PatrolPath.Mover mover;
+        internal AnimationController control;
+        internal Collider2D _collider;
+        SpriteRenderer spriteRenderer;
+
+        public Bounds Bounds => _collider.bounds;
+
+        void Awake()
+        {
+            control = GetComponent<AnimationController>();
+            _collider = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                var ev = Schedule<PlayerEnemyCollision>();
+                ev.player = player;
+                ev.enemy = this;
+
+                // Play an ouch sound when colliding with a player.
+                if (ouchEmitter != null)
+                {
+                    ouchEmitter.Play();
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (path != null)
+            {
+                if (mover == null) mover = path.CreateMover(control.maxSpeed * 0.5f);
+                control.move.x = Mathf.Clamp(mover.Position.x - transform.position.x, -1, 1);
+            }
+        }
+    }
+}
